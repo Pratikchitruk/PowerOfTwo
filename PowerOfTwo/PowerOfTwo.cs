a449dbbc5586e6d0713e109d0cc400e765b9a59f
PratikChitruk <pratik.chitruk@gmail.com>
2022-04-28 10:58:48 +0530

    Add PowerOfTwo

---
+++ PowerOfTwo/PowerOfTwo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PowerOfTwo 
+{
+    
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, mul = 1;
+
+            num = int.Parse(args[0]);
+
+            if (num > 31)
+            {
+                Console.WriteLine("Value Of number should be less than 31");
+                return;
+
+            }
+            else
+            {
+                Console.WriteLine(mul);
+                for (int j = 1; j <= num; j++)
+                {
+                    mul= mul * 2;
+                    Console.WriteLine(mul);
+
+                }
+            
+            
+            }
+
+            
+           
+        }
+    }
+}
